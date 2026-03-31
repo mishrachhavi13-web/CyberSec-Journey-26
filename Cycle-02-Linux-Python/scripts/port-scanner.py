3e55aec5b3df36d6ed8d4d0bcf95c8cbe851ddab
mishrachhavi13-web <mishrachhavi13@gmail.com>
2026-03-31 22:43:05 +0530

    Update port-scanner.py

--- Cycle-02-Linux-Python/scripts/port-scanner.py
+++ Cycle-02-Linux-Python/scripts/port-scanner.py
@@ -1,1 +1,21 @@
+import socket
+import time
 
+ip = input("Enter IP: ")
+
+start = time.time()
+
+for port in range(1, 1025):
+    s = socket.socket()
+    
+    try:
+        s.connect((ip, port))
+        print("Open port:", port)
+    except:
+        pass
+    
+    s.close()
+
+end = time.time()
+
+print("Time taken:", end - start)
