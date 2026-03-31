a26ec21b712cb079b9fa5c162a4ba9406c86e324
mishrachhavi13-web <mishrachhavi13@gmail.com>
2026-03-31 22:38:48 +0530

    Update file-analyzer.sh

--- Cycle-02-Linux-Python/scripts/file-analyzer.sh
+++ Cycle-02-Linux-Python/scripts/file-analyzer.sh
@@ -1,1 +1,25 @@
+#!/bin/bash<br>
+dir=$1
 
+echo "Directory is: $dir"<br>
+echo ""
+
+cd "$dir"
+
+echo "Files with sizes:"<br>
+ls -lh<br>
+echo ""
+
+
+count=$(ls | wc -l)<br>
+echo "Total files: $count"<br>
+echo ""
+
+
+echo "Total directory size:"<br>
+du -sh .<br>
+echo ""
+
+
+echo "Largest file:"<br>
+ls -lS | head -1
