a4e582351fb3a653c62459eaf6870b7c4402cb6f
mishrachhavi13-web <mishrachhavi13@gmail.com>
2026-03-31 22:47:01 +0530

    Update password-generator.py

--- Cycle-02-Linux-Python/scripts/password-generator.py
+++ Cycle-02-Linux-Python/scripts/password-generator.py
@@ -1,1 +1,12 @@
+import random
 
+length = int(input("Length: "))
+count = int(input("How many passwords: "))
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+
+for i in range(count):
+    password = ""
+    for j in range(length):
+        password += random.choice(chars)
+    print(password)
